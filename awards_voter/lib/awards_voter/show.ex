f7bc2d2baeed93c5d4fff8ff0af96b26d5dd754f
Akosua Asante <akosuaasante@gmail.com>
2019-11-24 01:55:00 -0500

    show struct initial commit

---
+++ awards_voter/lib/awards_voter/show.ex
@@ -0,0 +1,11 @@
+defmodule AwardsVoter.Show do
+  alias __MODULE__
+  
+  @enforce_keys [:name]
+  defstruct [:name, :categories]
+  @type t :: %__MODULE__{name: String.t(), categories: list()}
+  
+  def new(name, categories) do
+    {:ok, %Show{name: name, categories: categories}}
+  end
+end
