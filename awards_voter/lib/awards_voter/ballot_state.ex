be99946be527b592122a72c5b165574ea4727a36
Akosua Asante <akosuaasante@gmail.com>
2019-11-26 11:09:32 -0500

    basic ballot state machine

---
+++ awards_voter/lib/awards_voter/ballot_state.ex
@@ -0,0 +1,54 @@
+defmodule AwardsVoter.BallotState do
+  alias __MODULE__
+  
+  defstruct status: :initialized, ballot: nil, show: nil
+  
+  def new(), do: %BallotState{}
+
+  def check(%BallotState{status: :initialized} = state, :set_show) do
+    {:ok, %{state | status: :show_set}}
+  end
+
+  def check(%BallotState{status: :show_set} = state, :set_show) do
+    {:ok, %{state | status: :show_set}}
+  end
+  
+  def check(%BallotState{status: :show_set} = state, :set_ballot) do
+    {:ok, %{state | status: :ballot_set}}
+  end
+  
+  def check(%BallotState{status: :ballot_set} = state, :set_ballot) do
+    {:ok, %{state | status: :ballot_set}}
+  end
+
+  def check(%BallotState{status: :ballot_set} = state, :vote) do
+    {:ok, %{state | status: :voting}}
+  end
+
+  def check(%BallotState{status: :voting} = state, :vote) do
+    {:ok, %{state | status: :voting}}
+  end
+
+  def check(%BallotState{status: :voting} = state, :submit) do
+    {:ok, %{state | status: :submitted}}
+  end
+
+  def check(%BallotState{status: :submitted} = state, :revote) do
+    {:ok, %{state | status: :voting}}
+  end
+
+  def check(%BallotState{status: :submitted} = state, :submit) do
+    {:ok, %{state | status: :submitted}}
+  end
+
+  def check(%BallotState{status: :voting} = state, :end_show) do
+    {:ok, %{state | status: :show_ended}}
+  end
+
+  def check(%BallotState{status: :submitted} = state, :end_show) do
+    {:ok, %{state | status: :show_ended}}
+  end
+  
+  def check(_state, _action), do: :error
+
+end
