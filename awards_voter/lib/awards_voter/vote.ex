096348f99c8106d165ff917c4c7a358224aa713d
Akosua Asante <aaasante@uwaterloo.ca>
2020-01-04 12:53:46 -0500

    updating vote for tests and dialyzer

--- awards_voter/lib/awards_voter/vote.ex
+++ awards_voter/lib/awards_voter/vote.ex
@@ -16,10 +16,10 @@ defmodule AwardsVoter.Vote do
   def is_winning_vote?(%Vote{category: nil}), do: false
   def is_winning_vote?(%Vote{category: %Category{winner: nil}}), do: false
   def is_winning_vote?(vote) do
-    vote.contestant == vote.category.winner
+    vote.contestant.name == vote.category.winner.name
   end
 
-  @spec vote(Vote.t(), String.t()) :: :error | {:ok, Vote.t()} | {:invalid_vote, Vote.t()}
+  @spec vote(Vote.t(), String.t()) :: :invalid_category | {:ok, Vote.t()} | {:invalid_vote, Vote.t()}
   def vote(nil, _), do: :error
   def vote(vote, contestant_name) do
     case Enum.find(vote.category.contestants, nil, fn contestant ->
