1f1c43539ef6568241bc9b71579c0105329878e6
Akosua Asante <aaasante@uwaterloo.ca>
2020-02-09 13:54:33 -0500

    add some logging

--- lib/web/live/scoreboard_view.ex
+++ lib/web/live/scoreboard_view.ex
@@ -1,5 +1,7 @@
 defmodule AwardsVoter.Web.ScoreboardView do
   use Phoenix.LiveView
+  
+  require Logger
 
   alias AwardsVoter.Context.Voting
   
@@ -9,12 +11,14 @@ defmodule AwardsVoter.Web.ScoreboardView do
   
   def mount(session, socket) do
     show_name = session["show_name"]
+    Logger.debug("Mounting scoreboard for: #{show_name}")
     if connected?(socket), do: AwardsVoter.Web.Endpoint.subscribe("ballots:#{URI.encode(show_name)}")
     scores = Voting.get_scores_for_show(show_name)
     {:ok, assign(socket, scores: scores, show_name: show_name)}
   end
   
   def handle_info(%{event: "update_scores", topic: "ballots:" <> show_name} = chanel, socket) do
+    Logger.debug("Handling update_scores broadcast for: #{show_name}")
     show_name = URI.decode(show_name)
     scores = Voting.get_scores_for_show(show_name)
     {:noreply, assign(socket, scores: scores)}
