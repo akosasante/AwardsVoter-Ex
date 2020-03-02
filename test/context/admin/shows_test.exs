866a738941122e6e23e1407e15c3a2c619e5b3fa
Akosua Asante <aaasante@uwaterloo.ca>
2020-03-01 22:07:01 -0500

    update shows_tests module with new application.put_env strategy

--- test/context/admin/shows_test.exs
+++ test/context/admin/shows_test.exs
@@ -3,23 +3,20 @@ defmodule AwardsVoter.Context.Admin.ShowTest do
 
   alias AwardsVoter.Context.Admin
   alias AwardsVoter.Context.Admin.Shows
-  
+
   describe "Shows.create_show/2" do
     test "calls save_or_update_shows method if changeset is valid" do
       defmodule ShowModuleCreateValid do
-        def save_or_update_shows(show) do
-          expected = test_show()
-          assert ^expected = show
-          {:ok, show}
-        end
+        def put(show_tuples), do: :ok
       end
-      
+      Application.put_env(:awards_voter, :show_manager_mod, ShowModuleCreateValid)
       show_map = test_show() |> Admin.show_to_map()
+
       refute Map.get(show_map, :__struct__)
-      assert {:ok, saved_show} = Shows.create_show(show_map, ShowModuleCreateValid)
+      assert {:ok, saved_show} = Shows.create_show(show_map)
       assert ^saved_show = test_show()
     end
-    
+
     test "returns changeset with action :create and errors if invalid" do
       {:errors, cs} = Shows.create_show(%{name: nil, categories: []})
       assert %Ecto.Changeset{} = cs
@@ -31,43 +28,35 @@ defmodule AwardsVoter.Context.Admin.ShowTest do
   describe "Shows.update_show/3" do
     test "calls only save_or_update_shows method if name has not been changed" do
       defmodule ShowModuleUpdateExceptName do
-        def save_or_update_shows(show) do
-          expected = test_show() |> Map.update!(:categories, fn _ -> [] end)
-          assert ^expected = show
-          {:ok, show}
-        end
+        def put(show_tuple), do: :ok
       end
-      
+      Application.put_env(:awards_voter, :show_manager_mod, ShowModuleUpdateExceptName)
       updated_show = test_show() |> Map.update!(:categories, fn _ -> [] end)
       updated_show_attrs = updated_show  |> Admin.show_to_map()
       refute Map.get(updated_show_attrs, :__struct__)
-      
-      {:ok, saved_show} = Shows.update_show(test_show(), updated_show_attrs, ShowModuleUpdateExceptName)
+
+      {:ok, saved_show} = Shows.update_show(test_show(), updated_show_attrs)
       assert ^saved_show = updated_show
     end
-    
+
     test "call delete then save_or_update if name has been changed" do
       defmodule ShowModuleUpdateNameChange do
-        def save_or_update_shows(show) do
-          expected = test_show() |> Map.update!(:name, fn _ -> "Updated Name" end)
-          assert ^expected = show
-          {:ok, show}
-        end
-        
-        def delete_show_entry(show_name) do
+        def put(show_tuple), do: :ok
+
+        def delete(show_name) do
           assert ^show_name = test_show().name
           :ok
         end
       end
-        
+      Application.put_env(:awards_voter, :show_manager_mod, ShowModuleUpdateNameChange)
       updated_show = test_show() |> Map.update!(:name, fn _ -> "Updated Name" end)
       updated_show_attrs = updated_show  |> Admin.show_to_map()
       refute Map.get(updated_show_attrs, :__struct__)
 
-      {:ok, saved_show} = Shows.update_show(test_show(), updated_show_attrs, ShowModuleUpdateNameChange)
+      {:ok, saved_show} = Shows.update_show(test_show(), updated_show_attrs)
       assert ^saved_show = updated_show
     end
-    
+
     test "returns changeset with action :update and errors if invalid" do
       {:errors, cs} = Shows.update_show(test_show(), %{name: nil, categories: []})
       assert %Ecto.Changeset{} = cs
@@ -75,29 +64,31 @@ defmodule AwardsVoter.Context.Admin.ShowTest do
       assert cs.errors
     end
   end
-  
+
   describe "Shows.delete_show/2" do
     test "calls delete_show_entry and returns positive tuple if successful" do
       defmodule ShowModuleDelete do
-        def delete_show_entry(show_name) do
+        def delete(show_name) do
           assert ^show_name = test_show().name
           :ok
         end
       end
-      
+      Application.put_env(:awards_voter, :show_manager_mod, ShowModuleDelete)
       show = test_show()
-      assert {:ok, ^show} = Shows.delete_show(show, ShowModuleDelete)
+
+      assert {:ok, ^show} = Shows.delete_show(show)
     end
-    
+
     test "calls delete_show_entry and returns error tuple if call fails" do
       defmodule ShowModuleDeleteError do
-        def delete_show_entry(_show_name) do
-          {:delete_error, :reason}
+        def delete(_show_name) do
+          {:error, :reason}
         end
       end
-      
+      Application.put_env(:awards_voter, :show_manager_mod, ShowModuleDeleteError)
       show = test_show()
-      assert {:error, {:delete_error, :reason}} = Shows.delete_show(show, ShowModuleDeleteError)
+
+      assert {:error, :error_deleting} = Shows.delete_show(show)
     end
   end
 end
