dae449417e6d07f96924d3ef5eea331aa2ee76cf
Akosua Asante <aaasante@uwaterloo.ca>
2020-08-30 20:34:42 -0400

    wip: debugging scoreboard page

--- lib/web/live/scoreboard_view.ex
+++ lib/web/live/scoreboard_view.ex
@@ -1,24 +1,25 @@
 defmodule AwardsVoter.Web.ScoreboardView do
   use Phoenix.LiveView
-  
+
   require Logger
 
   alias AwardsVoter.Context.Voting
-  
+
   def render(assigns) do
+    Logger.info("Rendering scoreboard: #{inspect assigns}")
     AwardsVoter.Web.BallotView.render("scoreboard.html", assigns)
   end
-  
+
   def mount(session, socket) do
     show_name = session["show_name"]
-    Logger.debug("Mounting scoreboard for: #{show_name}")
+    Logger.info("Mounting scoreboard for: #{show_name}")
     if connected?(socket), do: AwardsVoter.Web.Endpoint.subscribe("ballots:#{URI.encode(show_name)}")
     scores = Voting.get_scores_for_show(show_name)
     {:ok, assign(socket, scores: scores, show_name: show_name)}
   end
-  
+
   def handle_info(%{event: "winner_updated", topic: "ballots:" <> show_name} = _channel, socket) do
-    Logger.debug("Handling winner_updated broadcast for: #{show_name}")
+    Logger.info("Handling winner_updated broadcast for: #{show_name}")
     show_name = URI.decode(show_name)
     scores = Voting.get_scores_for_show(show_name)
     {:noreply, assign(socket, scores: scores)}
