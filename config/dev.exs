ab12d8aabcea2e56633956fa12829a64a565f27c
Akosua Asante <aaasante@uwaterloo.ca>
2020-09-17 17:51:45 -0400

    https dev config

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,12 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :awards_voter, AwardsVoter.Web.Endpoint,
-  http: [port: 4000],
+  https: [
+   port: 4001,
+   cipher_suite: :strong,
+   keyfile: "priv/cert/selfsigned_key.pem",
+   certfile: "priv/cert/selfsigned.pem"
+  ],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
