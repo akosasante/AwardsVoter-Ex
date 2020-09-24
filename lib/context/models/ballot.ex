c7bfaee4baf970feb72831460da8a526f9ee4119
Akosua Asante <aaasante@uwaterloo.ca>
2020-09-23 22:26:00 -0400

    add CRUD methods (CU) for ballot

--- lib/context/models/ballot.ex
+++ lib/context/models/ballot.ex
@@ -7,6 +7,7 @@ defmodule AwardsVoter.Context.Models.Ballot do
 
   alias __MODULE__
   alias AwardsVoter.Context.Models.Vote
+  alias Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: true}
 
@@ -14,9 +15,10 @@ defmodule AwardsVoter.Context.Models.Ballot do
           voter: String.t() | nil,
           votes: list(Vote.t())
         }
+  @type change_result :: {:ok, Ballot.t()} | {:errors, Changeset.t()}
 
   embedded_schema do
-    embeds_one :voter, :string
+    field :voter, :string
     embeds_many :votes, Vote, on_replace: :delete
   end
 
@@ -26,5 +28,34 @@ defmodule AwardsVoter.Context.Models.Ballot do
     |> cast(attrs, [:voter])
     |> validate_required([:voter])
     |> cast_embed(:votes)
+  end
+
+  @spec to_changeset(Ballot.t()) :: Changeset.t()
+  def to_changeset(%Ballot{} = ballot) do
+    Ballot.changeset(ballot, %{})
+  end
+
+  @spec create(map()) :: change_result()
+  def create(attrs \\ %{}) do
+    cs = Ballot.changeset(%Ballot{}, attrs)
+
+    if cs.valid? do
+      {:ok, Changeset.apply_changes(cs)}
+    else
+      cs = %{cs | action: :create}
+      {:errors, cs}
+    end
+  end
+
+  @spec update(Ballot.t(), map()) :: change_result()
+  def update(%Ballot{} = orig_ballot, attrs) do
+    cs = Ballot.changeset(orig_ballot, attrs)
+
+    if cs.valid? do
+      {:ok, Changeset.apply_changes(cs)}
+    else
+      cs = %{cs | action: :update}
+      {:errors, cs}
+    end
   end
 end
--- test/context/models/ballot_test.exs
+++ test/context/models/ballot_test.exs
@@ -1,20 +1,61 @@
 defmodule AwardsVoter.Context.Models.BallotTest do
   use AwardsVoter.DataCase
 
-  alias AwardsVoter.Context.Models.Show
+  alias AwardsVoter.Context.Models.Ballot
 
-  test "should return valid changeset if params are valid" do
-    cs = Show.changeset(%Show{}, params_for(:show))
+  describe "Ballot.changeset/2" do
+    test "should return valid changeset if params are valid" do
+      cs = Ballot.changeset(%Ballot{}, params_for(:ballot))
 
-    assert cs.valid?
-    assert %Ecto.Changeset{} = cs
+      assert cs.valid?
+      assert %Ecto.Changeset{} = cs
+    end
+
+    test "should return errors if the required params are not provided" do
+      ballot = params_for(:ballot) |> Map.update!(:voter, fn _ -> nil end)
+      cs = Ballot.changeset(%Ballot{}, ballot)
+
+      refute cs.valid?
+      assert errors_on(cs) == %{voter: ["can't be blank"]}
+    end
   end
 
-  test "should return errors if the required params are not provided" do
-    show = params_for(:show) |> Map.update!(:name, fn _ -> nil end)
-    cs = Show.changeset(%Show{}, show)
+  describe "Ballot.create/2" do
+    test "returns applied changes if changeset is valid" do
+      assert {:ok, ballot} = Ballot.create(params_for(:ballot))
+      assert ballot = params_for(:ballot)
+    end
 
-    refute cs.valid?
-    assert errors_on(cs) == %{name: ["can't be blank"]}
+    test "returns changeset errors if changeset is invalid" do
+      ballot = params_for(:ballot) |> Map.update!(:voter, fn _ -> nil end)
+
+      assert {:errors, cs} = Ballot.create(ballot)
+      refute cs.valid?
+      assert cs.action == :create
+      assert errors_on(cs) == %{voter: ["can't be blank"]}
+    end
+  end
+
+  describe "Ballot.update/2" do
+    test "returns applied changes if changeset is valid" do
+      orig_ballot_map = params_for(:ballot)
+      updated_ballot_map = %{orig_ballot_map | voter: "Updated Ballot"}
+      {:ok, updated_ballot} = Ballot.create(updated_ballot_map)
+      {:ok, orig_ballot} = Ballot.create(orig_ballot_map)
+
+      assert {:ok, ballot} = Ballot.update(orig_ballot, updated_ballot_map)
+      assert ^ballot = updated_ballot
+    end
+
+    test "returns changeset errors if changeset is invalid" do
+      orig_ballot_map = params_for(:ballot)
+      updated_ballot = %{orig_ballot_map | voter: nil}
+      {:ok, orig_ballot} = Ballot.create(orig_ballot_map)
+
+      assert {:errors, cs} = Ballot.update(orig_ballot, updated_ballot)
+      refute cs.valid?
+      assert cs.action == :update
+      assert errors_on(cs) == %{voter: ["can't be blank"]}
+    end
   end
 end
