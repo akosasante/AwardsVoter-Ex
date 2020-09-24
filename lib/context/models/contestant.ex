0f503a6c2acfa62efd6826876253acae8a02e0fd
Akosua Asante <aaasante@uwaterloo.ca>
2020-09-23 21:48:15 -0400

    add CRUD methods (CU) for contestant

--- lib/context/models/contestant.ex
+++ lib/context/models/contestant.ex
@@ -6,9 +6,11 @@ defmodule AwardsVoter.Context.Models.Contestant do
   import Ecto.Changeset
 
   alias __MODULE__
+  alias Ecto.Changeset
 
   @primary_key {:id, :binary_id, autogenerate: true}
 
+  @type change_result :: {:ok, Contestant.t()} | {:errors, Changeset.t()}
   @type t :: %__MODULE__{
           name: String.t() | nil,
           description: String.t() | nil,
@@ -42,5 +44,34 @@ defmodule AwardsVoter.Context.Models.Contestant do
       :billboard_stats
     ])
     |> validate_required([:name])
+  end
+
+  @spec to_changeset(Contestant.t()) :: Changeset.t()
+  def to_changeset(%Contestant{} = contestant) do
+    Contestant.changeset(contestant, %{})
+  end
+
+  @spec create(map()) :: change_result()
+  def create(attrs \\ %{}) do
+    cs = Contestant.changeset(%Contestant{}, attrs)
+
+    if cs.valid? do
+      {:ok, Changeset.apply_changes(cs)}
+    else
+      cs = %{cs | action: :create}
+      {:errors, cs}
+    end
+  end
+
+  @spec update(Contestant.t(), map()) :: change_result()
+  def update(%Contestant{} = orig_contestant, attrs) do
+    cs = Contestant.changeset(orig_contestant, attrs)
+
+    if cs.valid? do
+      {:ok, Changeset.apply_changes(cs)}
+    else
+      cs = %{cs | action: :update}
+      {:errors, cs}
+    end
   end
 end
--- test/context/models/contestant_test.exs
+++ test/context/models/contestant_test.exs
@@ -3,18 +3,56 @@ defmodule AwardsVoter.Context.Models.ContestantTest do
 
   alias AwardsVoter.Context.Models.Contestant
 
-  test "should return valid changeset if params are valid" do
-    cs = Contestant.changeset(%Contestant{}, params_for(:contestant))
+  describe "Contestant.changeset/2" do
+    test "should return valid changeset if params are valid" do
+      cs = Contestant.changeset(%Contestant{}, params_for(:contestant))
 
-    assert cs.valid?
-    assert %Ecto.Changeset{} = cs
+      assert cs.valid?
+      assert %Ecto.Changeset{} = cs
+    end
+
+    test "should return errors if the required params are not provided" do
+      contestant = params_for(:contestant) |> Map.update!(:name, fn _ -> nil end)
+      cs = Contestant.changeset(%Contestant{}, contestant)
+
+      refute cs.valid?
+      assert errors_on(cs) == %{name: ["can't be blank"]}
+    end
   end
 
-  test "should return errors if the required params are not provided" do
-    contestant = params_for(:contestant) |> Map.update!(:name, fn _ -> nil end)
-    cs = Contestant.changeset(%Contestant{}, contestant)
+  describe "Contestant.create/2" do
+    test "returns applied changes if changeset is valid" do
+      assert {:ok, contestant} = Contestant.create(params_for(:contestant))
+      assert contestant = params_for(:contestant)
+    end
 
-    refute cs.valid?
-    assert errors_on(cs) == %{name: ["can't be blank"]}
+    test "returns changeset errors if changeset is invalid" do
+      contestant = params_for(:contestant) |> Map.update!(:name, fn _ -> nil end)
+
+      assert {:errors, cs} = Contestant.create(contestant)
+      refute cs.valid?
+      assert cs.action == :create
+      assert errors_on(cs) == %{name: ["can't be blank"]}
+    end
+  end
+
+  describe "Contestant.update/2" do
+    test "returns applied changes if changeset is valid" do
+      orig_contestant = build(:contestant)
+      updated_contestant = %{orig_contestant | name: "Updated Contestant"} |> Map.from_struct()
+
+      assert {:ok, contestant} = Contestant.update(orig_contestant, updated_contestant)
+      assert ^contestant = struct!(%Contestant{}, updated_contestant)
+    end
+
+    test "returns changeset errors if changeset is invalid" do
+      orig_contestant = build(:contestant)
+      updated_contestant = %{orig_contestant | name: nil} |> Map.from_struct()
+
+      assert {:errors, cs} = Contestant.update(orig_contestant, updated_contestant)
+      refute cs.valid?
+      assert cs.action == :update
+      assert errors_on(cs) == %{name: ["can't be blank"]}
+    end
   end
 end
