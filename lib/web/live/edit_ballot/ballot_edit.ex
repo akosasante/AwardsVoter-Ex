f9218c047f10af5897c29577b7538480a9f2c4f2
Akosua Asante <aaasante@uwaterloo.ca>
2020-12-16 00:02:45 -0500

    persist chosen votes in live view state

--- lib/web/live/edit_ballot/ballot_edit.ex
+++ lib/web/live/edit_ballot/ballot_edit.ex
@@ -13,6 +13,7 @@ defmodule AwardsVoter.Web.BallotEdit do
     socket =
       socket
       |> assign_new(:original_ballot, fn -> Ballots.get_ballot(ballot_id) end)
+      |> assign_new(:vote_map, fn -> Map.new() end)
 
     socket = assign_new(socket, :show, fn -> Admin.get_show_by_id(socket.assigns.original_ballot.show_id) end)
     socket = assign_new(socket, :current_category, fn -> socket.assigns.show.categories |> List.first() end)
@@ -24,6 +25,16 @@ defmodule AwardsVoter.Web.BallotEdit do
   def handle_params(%{"current_category" => current_category_name}, _uri, %{assigns: %{show: show}} = socket) do
     current_category = Admin.get_category_by_name(show, current_category_name)
     socket = assign(socket, :current_category, current_category)
+    {:noreply, socket}
+  end
+
+  def handle_event("update_vote", %{"vote" => vote}, %{assigns: %{current_category: category, vote_map: vote_map}} = socket) do
+    voted_contestant = Map.get(vote, category.name)
+    contestant = Admin.get_contestant_by_name(category, voted_contestant)
+
+    vote_map = Map.put(vote_map, category.name, voted_contestant)
+    socket = assign(socket, :vote_map, vote_map)
+
     {:noreply, socket}
   end
 end
--- lib/web/views/ballot_view.ex
+++ lib/web/views/ballot_view.ex
@@ -1,4 +1,8 @@
 defmodule AwardsVoter.Web.BallotView do
-  use AwardsVoter.Web, :admin_view
+  use AwardsVoter.Web, :view
+
+  def is_matching_contestant(vote_map, category, contestant) do
+    Map.get(vote_map, category.name) == contestant.name
+  end
 
 end
