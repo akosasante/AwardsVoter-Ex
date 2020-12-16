777c9f7b29134923d0dc2e653b5bd062ad5ddc03
Akosua Asante <aaasante@uwaterloo.ca>
2020-12-15 23:31:57 -0500

    ballot edit page with live navigation

--- lib/web/live/edit_ballot/ballot_edit.ex
+++ lib/web/live/edit_ballot/ballot_edit.ex
@@ -15,7 +15,15 @@ defmodule AwardsVoter.Web.BallotEdit do
       |> assign_new(:original_ballot, fn -> Ballots.get_ballot(ballot_id) end)
 
     socket = assign_new(socket, :show, fn -> Admin.get_show_by_id(socket.assigns.original_ballot.show_id) end)
+    socket = assign_new(socket, :current_category, fn -> socket.assigns.show.categories |> List.first() end)
+    # Figure out how to set params on first view?
 
     {:ok, socket}
+  end
+
+  def handle_params(%{"current_category" => current_category_name}, _uri, %{assigns: %{show: show}} = socket) do
+    current_category = Admin.get_category_by_name(show, current_category_name)
+    socket = assign(socket, :current_category, current_category)
+    {:noreply, socket}
   end
 end
