409709dbb37d01d1ef3d45d8c9b6489caefe7985
Akosua Asante <aaasante@uwaterloo.ca>
2021-01-10 15:18:58 -0500

    get ballot page with proper links

--- lib/web/views/ballot_view.ex
+++ lib/web/views/ballot_view.ex
@@ -5,4 +5,10 @@ defmodule AwardsVoter.Web.BallotView do
     Map.get(vote_map, category.name) == contestant.name
   end
 
+  def get_voted_for_contestant(ballot, category) do
+    case Enum.find(ballot.votes, fn vote -> vote.category.name == category.name end) do
+      nil -> %{}
+      vote_for_category -> Map.get(vote_for_category, :contestant, nil)
+    end
+  end
 end
