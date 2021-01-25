891d6e032e7ff6118c3088419b56e03044ae00b6
Akosua Asante <aaasante@uwaterloo.ca>
2021-01-25 16:22:12 -0500

    summary view

--- lib/web/live/summaries/ballots_summary.ex
+++ lib/web/live/summaries/ballots_summary.ex
@@ -1,11 +1,22 @@
 defmodule AwardsVoter.Web.BallotsSummary do
   use Phoenix.LiveView
 
+  alias AwardsVoter.Context.Admin
+  alias AwardsVoter.Context.Ballots
+  alias AwardsVoter.Context.Models.Show
+
   def render(assigns) do
     AwardsVoter.Web.SummariesView.render("ballots_summary.html", assigns)
   end
 
-  def mount(_, _, socket) do
+  def mount(%{"id" => show_id}, _, socket) do
+    %Show{} = show = Admin.get_show_by_id(show_id)
+
+    socket =
+      socket
+      |> assign_new(:show, fn -> show end)
+      |> assign_new(:ballots, fn -> Ballots.fetch_ballots_for_show(show_id) end)
+
     {:ok, socket}
   end
 end
--- lib/web/views/summaries_view.ex
+++ lib/web/views/summaries_view.ex
@@ -3,6 +3,8 @@ defmodule AwardsVoter.Web.SummariesView do
 
   alias AwardsVoter.Context.Ballots
   alias AwardsVoter.Context.Models.Ballot
+  alias AwardsVoter.Context.Models.Vote
+  alias AwardsVoter.Context.Models.Category
   alias AwardsVoter.Context.Models.Show
 
   def num_voted(%Ballot{votes: votes}) do
@@ -30,14 +32,45 @@ defmodule AwardsVoter.Web.SummariesView do
     end
   end
 
-  def num_winners(%Show{categories: categories}) do
-    Enum.count(categories, fn category -> !is_nil(category.winner) end)
+  def num_winners(%Show{} = show) do
+    Enum.count(winning_categories(show))
   end
 
   def num_categories(%Show{categories: categories}), do: Enum.count(categories)
 
-  defp num_correct(ballot) do
-    Ballots.count_correct_votes(ballot)
+  def winning_categories(%Show{categories: categories}), do: Enum.filter(categories, fn category -> !is_nil(category.winner) end)
+
+  def num_correct_voted_for_category(show, category, ballots) do
+    Enum.count(ballots, fn %Ballot{votes: votes} ->
+      case Enum.find(votes, fn vote -> vote.category.name == category.name end) do
+        %Vote{contestant: contestant} -> contestant.name == category.winner.name
+        _ -> false
+      end
+    end)
   end
+
+  def percent_correct_voted_for_category(show, category, ballots) do
+    num_ballots_with_vote_for_category = Enum.count(ballots, fn %Ballot{votes: votes} -> Enum.find(votes, fn vote -> vote.category.name == category.name end) end)
+    num_ballots_with_correct_votes = num_correct_voted_for_category(show, category, ballots)
+
+    if num_ballots_with_vote_for_category == 0 or num_ballots_with_correct_votes == 0 do
+      "0.00"
+    else
+      div(num_ballots_with_correct_votes, num_ballots_with_vote_for_category) |> div(1) |> Float.round(2)
+    end
+  end
+
+  def most_common_vote(show, %Category{contestants: contestants, name: category_name}, ballots) do
+    Enum.frequencies_by(ballots, fn %Ballot{votes: votes} ->
+      case Enum.find(votes, fn vote -> vote.category.name == category_name end) do
+        %Vote{contestant: contestant} -> contestant.name
+        _ -> :not_vote
+      end
+    end)
+    |> Map.drop([:not_vote])
+    |> Enum.max_by(fn {_, num_votes} -> num_votes end)
+  end
+
+  def num_correct(ballot), do: Ballots.count_correct_votes(ballot)
 
 end
