e73ffcb82c98db8c47ba5a515bdf54ecf3febe8e
Akosua Asante <aaasante@uwaterloo.ca>
2021-02-02 17:34:06 -0500

    update home page title bar

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -29,6 +29,58 @@ module.exports = {
         },
       },
     }),
+    container: {
+      padding: "2rem"
+    },
+    fontSize: {
+      xs: ".75rem",
+      sm: ".875rem",
+      tiny: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
+      "8xl": "6rem",
+      "9xl": "7rem",
+      "10xl": "8rem"
+    },
+    extend: {
+      spacing: {
+        "72": "18rem",
+        "84": "21rem",
+        "96": "24rem"
+      },
+      maxWidth: {
+        "1/4": "25%",
+        "1/2": "50%",
+        "3/4": "75%",
+        "9/10": "90%"
+      },
+      translate: {
+        double: "200%",
+        triple: "300%",
+        quad: "400%"
+      },
+      height: {
+        "2px": "2px"
+      },
+      inset: {
+        "24": "5rem", // not for real
+        "1/2": "50%",
+        full: "100%"
+      },
+      transitionProperty: {
+        width: "width"
+      },
+      fontFamily: {
+        'title': ['Limelight', 'ui-sans-serif', 'system-ui']
+      }
+    }
   },
   variants: {},
   plugins: [
