db059fc1007c7fbe2614604c7cbc75626978fcce
Akosua Asante <aaasante@uwaterloo.ca>
2021-02-20 13:25:25 -0500

    gigalixir socket stuff

--- config/prod.exs
+++ config/prod.exs
@@ -12,6 +12,8 @@ import Config
 config :awards_voter, AwardsVoter.Web.Endpoint,
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
+    url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+    check_origin: ["https://awards.akosua.xyz"],
     transport_options: [socket_opts: [:inet6]]
   ],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
